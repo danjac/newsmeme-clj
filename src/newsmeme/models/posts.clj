36be028d512b81be7d5509185769e2dd6d34d79d
Dan Jacob <danjac354@gmail.com>
2012-06-28 23:55:57 +0300

    Get tags for a post

--- src/newsmeme/models/posts.clj
+++ src/newsmeme/models/posts.clj
@@ -27,27 +27,7 @@
   [link]
   (and link 
        (first (select post (where {:link link})))))
-  
-(defn get-or-create-tag 
-  [slug]
-  (if-let [result (first (select tag (where {:slug slug})))]
-    (:id result)
-    (:id (insert tag (values {:slug slug})))))
 
-(defn insert-tagged
-  [post-id tag-id]
-  (insert tagged (values {:post_id post-id :tag_id tag-id})))
-
-(defn add-tags-to-post 
-  [post-id tag-ids]
-  ; delete all tags for this post
-  (delete tagged (where {:post_id post-id}))
-  (map #(insert-tagged post-id %) tag-ids))
-
-(defn tag-ids-from-string
-  [tags]
-  (let [tags (map #(string/trim %) (string/split tags #"\s"))]
-       (map #(get-or-create-tag %) tags)))
 
 (defn insert-post 
   "Insert a new post in the database" 
@@ -61,7 +41,43 @@
     (doall (add-tags-to-post (:id new-post) tag-ids)) new-post))
 
 (defn get-top-posts []
+  (select post (with user)(order :score :DESC :date_created :DESC)))
+
+
+(defn get-latest-posts []
   (select post (with user)(order :date_created :DESC)))
 
 
- 
+"Tagging functions"
+
+(defn get-or-create-tag 
+  [slug]
+  (if-let [result (first (select tag (where {:slug slug})))]
+    (:id result)
+    (:id (insert tag (values {:slug slug})))))
+
+(defn insert-tagged
+  [post-id tag-id]
+  (insert tagged (values {:post_id post-id :tag_id tag-id})))
+
+
+(defn add-tags-to-post 
+  [post-id tag-ids]
+  ; delete all tags for this post
+  (delete tagged (where {:post_id post-id}))
+  (map #(insert-tagged post-id %) tag-ids))
+
+(defn tag-ids-from-string
+  [tags]
+  (let [tags (map #(string/trim %) (string/split tags #"\s"))]
+       (map #(get-or-create-tag %) tags)))
+
+(defn get-tags-for-post
+  [post-id]
+  (map #(:slug %) 
+          (select tag
+          (join tagged (= :tags.id :post_tags.tag_id))
+          (where {:post_tags.post_id post-id}))))
+
+
+
